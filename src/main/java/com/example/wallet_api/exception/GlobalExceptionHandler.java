bc6624a03a1e4e2a1c491641b7691be26b7667a0
Zhidkov-Aleksandr <dotenvargen@gmail.com>
2026-01-21 14:34:44 +0300

    commit

--- src/main/java/com/example/wallet_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/wallet_api/exception/GlobalExceptionHandler.java
@@ -6,9 +6,17 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvalidDataAccessApiUsageException.class)
+    public ResponseEntity<ApiError> handleDataAccess() {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ApiError("INVALID_REQUEST"));
+    }
 
     @ExceptionHandler(WalletNotFoundException.class)
     public ResponseEntity<ApiError> handleWalletNotFound() {
